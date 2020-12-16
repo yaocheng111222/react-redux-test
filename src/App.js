c39f5f56ddf81966f348e02601f6dc560fe289aa
yaocheng <1075168952@qq.com>
2020-12-16 11:19:31 +0800

    创建react项目并使用redux react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+// 也可以使用无状态组件
+class App1 extends Component {
+    render() {
+        // 用解构赋值
+        let { inputValue, inputChange, clickBtn, list, deleteItem } = this.props
+        return (
+            <div>
+                <div>
+                    <input 
+                        value={inputValue} 
+                        onChange={inputChange} />
+                    <button onClick={clickBtn}>提交</button>
+                </div>
+                <ul>
+                    {list.map((item,index) => (<li key={index} onClick={()=>deleteItem(index)}>{item}</li>))}
+                </ul>
+            </div>
+        );
+    }
 }
-
-export default App;
+const App = (props) => {
+    let { inputValue, inputChange, clickBtn, list, deleteItem } = props
+    return (
+        <div>
+            <div>
+                <input 
+                    value={inputValue} 
+                    onChange={inputChange} />
+                <button onClick={clickBtn}>提交</button>
+            </div>
+            <ul>
+                {list.map((item,index) => (<li key={index} onClick={()=>deleteItem(index)}>{item}</li>))}
+            </ul>
+        </div>
+    )
+}
+const stateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
+    }
+}
+const dispatchToProps = (dispatch) =>{
+    return {
+        inputChange(e){
+            let action = {
+                type: 'change_input',
+                value: e.target.value
+            }
+            dispatch(action)
+        },
+        clickBtn() {
+            let action = {
+                type: 'add_item',
+            }
+            dispatch(action)
+        },
+        deleteItem(index) {
+            let action = {
+                type: 'delete_item',
+                index
+            }
+            dispatch(action)
+        }
+    }
+}
+ 
+export default connect(stateToProps,dispatchToProps)(App);
