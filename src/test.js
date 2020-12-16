3b15abd877b413995e0bcf4549db20616a6459e6
yaocheng <1075168952@qq.com>
2020-12-16 11:27:31 +0800

    测试dev

---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Test extends Component {
+    render() { 
+        return (
+            <div>222</div>
+        );
+    }
+}
+ 
+export default Test;
